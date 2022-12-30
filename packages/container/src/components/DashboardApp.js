9f1350305325047540562e515336f5f8de854d07
Anderson <andersonrw16@hotmail.com>
2022-12-30 12:52:04 -0300

    dashboard app added to project

---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,12 @@
+import { mount } from "dashboard/DashboardApp";
+import React, { useRef, useEffect } from "react";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, [ref]);
+
+  return <div ref={ref} />;
+};
